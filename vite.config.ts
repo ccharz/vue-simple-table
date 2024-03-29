f3756a3a6bd75871b99cf9c036f487c8ed6b5b6a
Christian Charzewski <chris@charzewski.at>
2024-03-29 16:19:14 +0100

    Fixed typings

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,17 @@ import vue from '@vitejs/plugin-vue';
 
 import { resolve } from 'path';
 import dts from 'vite-plugin-dts';
+import { copyFileSync } from 'fs';
 
 export default defineConfig({
-    plugins: [vue(), dts()],
+    plugins: [
+        vue(),
+        dts({
+            afterBuild: () => {
+                copyFileSync('dist/index.d.ts', 'dist/index.d.cts');
+            },
+        }),
+    ],
     build: {
         lib: {
             entry: resolve(__dirname, 'src/index.ts'),
